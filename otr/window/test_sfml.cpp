095933e311e9d234924ab3378d2e446a038627c0
Adam Hlavatovic <sansajn@ymail.com>
2014-07-23 16:00:48 +0200

    window: sfml not yet ready

--- otr/window/test_sfml.cpp
+++ otr/window/test_sfml.cpp
@@ -1,84 +1,84 @@
-#include <iostream>
-#include <GL/glew.h>
-#include "ui/sfml_window.h"
+//#include <iostream>
+//#include <GL/glew.h>
+//#include "ui/sfml_window.h"
 
-using std::cout;
+//using std::cout;
 
-using namespace gl;
+//using namespace gl;
 
-class app_window
-	: public sfml_window
-{
-public:
-	app_window()
-		: sfml_window(window::parameters().size(800, 600).name("sfml window"))
-	{
-		glClearColor(.5f, .5f, .5f, 1.0f);
-	}
+//class app_window
+//	: public sfml_window
+//{
+//public:
+//	app_window()
+//		: sfml_window(window::parameters().size(800, 600).name("sfml window"))
+//	{
+//		glClearColor(.5f, .5f, .5f, 1.0f);
+//	}
 
-	void display()
-	{
-		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-		sdl_window::display();
-	}
+//	void display()
+//	{
+//		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+//		sdl_window::display();
+//	}
 
-/*
-	void mouse_motion(int x, int y)
-	{
-		cout << "mouse_motion(x:" << x << ", y:" << y << ")\n";
-		sdl_window::mouse_motion(x, y);
-	}
+///*
+//	void mouse_motion(int x, int y)
+//	{
+//		cout << "mouse_motion(x:" << x << ", y:" << y << ")\n";
+//		sdl_window::mouse_motion(x, y);
+//	}
 
-	void mouse_passive_motion(int x, int y)
-	{
-		cout << "mouse_passive_motion(x:" << x << ", y:" << y << ")\n";
-		sdl_window::mouse_passive_motion(x, y);
-	}
+//	void mouse_passive_motion(int x, int y)
+//	{
+//		cout << "mouse_passive_motion(x:" << x << ", y:" << y << ")\n";
+//		sdl_window::mouse_passive_motion(x, y);
+//	}
 
-	void mouse_wheel(wheel b, modifier m, int x, int y)
-	{
-		cout << "mouse_wheel(b:" << (b == wheel::up ? "up" : "down") << ")\n";
-		sdl_window::mouse_wheel(b, m, x, y);
-	}
+//	void mouse_wheel(wheel b, modifier m, int x, int y)
+//	{
+//		cout << "mouse_wheel(b:" << (b == wheel::up ? "up" : "down") << ")\n";
+//		sdl_window::mouse_wheel(b, m, x, y);
+//	}
 
-	void mouse_click(button b, state s, modifier m, int x, int y)
-	{
-		cout << "mouse_click(b:" << button_name(b)
-			<< ", s:" << (s == state::down ? "down" : "up")
-			<< ", x:" << x << ", y:" << y << ")\n";
-		sdl_window::mouse_click(b, s, m, x, y);
-	}
+//	void mouse_click(button b, state s, modifier m, int x, int y)
+//	{
+//		cout << "mouse_click(b:" << button_name(b)
+//			<< ", s:" << (s == state::down ? "down" : "up")
+//			<< ", x:" << x << ", y:" << y << ")\n";
+//		sdl_window::mouse_click(b, s, m, x, y);
+//	}
 
-	void key_typed(unsigned char c, modifier m, int x, int y)
-	{
-		cout << "key_typed(c:" << c << ")\n";
-		sdl_window::key_typed(c, m, x, y);
-	}
+//	void key_typed(unsigned char c, modifier m, int x, int y)
+//	{
+//		cout << "key_typed(c:" << c << ")\n";
+//		sdl_window::key_typed(c, m, x, y);
+//	}
 
-	void key_released(unsigned char c, modifier m, int x, int y)
-	{
-		cout << "key_released(c:" << c << ")\n";
-		sdl_window::key_released(c, m, x, y);
-	}
+//	void key_released(unsigned char c, modifier m, int x, int y)
+//	{
+//		cout << "key_released(c:" << c << ")\n";
+//		sdl_window::key_released(c, m, x, y);
+//	}
 
-	void special_key(key k, modifier m, int x, int y)
-	{
-		cout << "special_key(k:" << key_name(k) << ")\n";
-		sdl_window::special_key(k, m, x, y);
-	}
+//	void special_key(key k, modifier m, int x, int y)
+//	{
+//		cout << "special_key(k:" << key_name(k) << ")\n";
+//		sdl_window::special_key(k, m, x, y);
+//	}
 
-	void special_key_released(key k, modifier m, int x, int y)
-	{
-		cout << "special_key_released(k:" << key_name(k) << ")\n";
-		sdl_window::special_key_released(k, m, x, y);
-	}
-*/
-};
+//	void special_key_released(key k, modifier m, int x, int y)
+//	{
+//		cout << "special_key_released(k:" << key_name(k) << ")\n";
+//		sdl_window::special_key_released(k, m, x, y);
+//	}
+//*/
+//};
 
 
 int main(int argc, char * argv[])
 {
-	app_window w;
-	w.start();
+//	app_window w;
+//	w.start();
 	return 0;
 }
--- otr/window/ui/sfml_window.h
+++ otr/window/ui/sfml_window.h
@@ -1,29 +1,29 @@
 #pragma once
-#include <glm/glm.hpp>
-#include <SFML/Window.hpp>
-#include "window.h"
+//#include <glm/glm.hpp>
+//#include <SFML/Window.hpp>
+//#include "window.h"
 
-namespace gl {
+//namespace gl {
 
-class sfml_window
-	: public window
-{
-public:
-	sfml_window(parameters const & params);
+//class sfml_window
+//	: public window
+//{
+//public:
+//	sfml_window(parameters const & params);
 
-	void start();
+//	void start();
 
-	void display() {}
-	void reshape(int w, int h) {}
+//	void display() {}
+//	void reshape(int w, int h) {}
 
-private:
-	void handle_events();
-	void handle_mouse(sf::Event & e);
-	void handle_keyboard(sf::Event & e);
+//private:
+//	void handle_events();
+//	void handle_mouse(sf::Event & e);
+//	void handle_keyboard(sf::Event & e);
 
-	sf::Window _window;
-	bool _mouse_down;
-	glm::vec2 _mouse_pos;
-};
+//	sf::Window _window;
+//	bool _mouse_down;
+//	glm::vec2 _mouse_pos;
+//};
 
-}  // gl
+//}  // gl
