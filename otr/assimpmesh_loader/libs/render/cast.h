b8e07dedb656d450e359976a23606b33ce3f5968
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2014-10-22 02:42:43 +0200

    removes unused

--- otr/assimpmesh_loader/libs/render/cast.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <GL/glew.h>
-#include "render/types.h"
-
-GLenum ogl_cast(mesh_mode m);
-GLenum ogl_cast(attribute_type t);
-GLenum ogl_cast(polygon_raster_mode r);
--- otr/orkmesh_loader/libs/render/cast.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <GL/glew.h>
-#include "types.h"
-
-GLenum ogl_cast(mesh_mode m);
-GLenum ogl_cast(polygon_rasterization r);
-GLenum ogl_cast(attribute_type t);
-GLenum ogl_cast(buffer_usage u);
