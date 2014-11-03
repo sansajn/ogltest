028bfacfe1c3352f3f35b5afc8a7e15fa424d5d2
Adam Hlavatovic <sansajn@ymail.com>
2014-11-03 12:28:14 +0100

    resource manager implementation (no loader)

--- otr/resource/main.cpp
+++ otr/resource/main.cpp
@@ -3,26 +3,15 @@
 #include "resource/resource_manager.hpp"
 
 
-class string_res : public resource
-{
-public:
-	string_res(std::string const & name, std::string const & content)
-		: resource(name), _content(content)
-	{}
-
-	std::string const & data() const {return _content;}
-
-private:
-	std::string _content;
-};
-
-
 int main(int argc, char * argv[])
 {
 	resource_manager man;
-	ptr<string_res> r(new string_res("user", "Teresa Lisbon"));
-	man.insert_resource(r->name(), r);
-	auto s = man.load_resource<string_res>("user");
-	std::cout << s->name() << ":" << s->data() << std::endl;
+	ptr<std::string> r(new std::string("Teresa Lisbon"));
+	man.insert_resource("user", r);
+	auto s = man.load_resource<std::string>("user");
+	if (s)
+		std::cout << "user:" << *s << std::endl;
+	else
+		std::cout << "user is not an instance of requested-type" << std::endl;
 	return 0;
 }
---
+++ otr/resource/readme.txt
@@ -0,0 +1,2 @@
+no v podstate bez loaderu je resource manager iba mapa, ktorá asociuje zdroj s
+klúčom.
