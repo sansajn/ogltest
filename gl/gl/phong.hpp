fdef98553db0db232eacdf86f6f68140e048de93
Adam Hlavatovic <adam.hlavatovic@gmail.com>
2015-12-10 22:28:55 +0100

    gl: phong removes unused

--- gl/gl/phong.hpp
+++ gl/gl/phong.hpp
@@ -20,7 +20,6 @@ public:
 	phong_light() {}
 	void init(shader::program * p, gl::camera const * cam);
 	void render(gl::mesh const & m, glm::mat4 const & local_to_world);
-	void attach_camera(gl::camera const * cam);
 	void light(glm::vec3 const & direction, glm::vec3 const & color, float intensity);  //!< \param[in] direction ide o normalizovany vektor
 	void material(glm::vec3 const & ambient, float shininess, float intensity);
 	void light_direction(glm::vec3 const & dir);
