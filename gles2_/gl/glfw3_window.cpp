81555a204704025f20be13c7d90fe60b0fc060e8
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2018-11-19 16:41:28 +0100

    gles2_: glfw window input support

--- gles2_/gl/glfw3_window.cpp
+++ gles2_/gl/glfw3_window.cpp
@@ -1,8 +1,11 @@
 #include "glfw3_window.hpp"
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 namespace ui {
 
+using glm::ivec2;
 
 namespace glfw_detail {
 
@@ -63,6 +66,123 @@ void glfw3_layer::main_loop_event()
 	if (glfwWindowShouldClose(glfw_detail::__glfw_window))
 		close();
 }
+
+
+glfw3_layer::user_input::user_input()
+{
+	for (unsigned i = 0; i < NUM_KEYS; ++i)
+		_keys[i] = _keys_up[i] = false;
+
+	_mouse_pos = glm::vec2(0,0);  // TODO: zvaz hodnotu center
+	for (unsigned i = 0; i < NUM_BUTTONS; ++i)
+		_mouse_buttons[i] = _mouse_buttons_up[i] = false;
+}
+
+bool glfw3_layer::user_input::key(unsigned char c) const
+{
+	return _keys[c];
+}
+
+bool glfw3_layer::user_input::key_up(unsigned char c) const
+{
+	return _keys_up[c];
+}
+
+bool glfw3_layer::user_input::any_of_key(char const * s) const
+{
+	while (*s)
+		if (key(*s++))
+			return true;
+	return false;
+}
+
+bool glfw3_layer::user_input::any_of_key_up(char const * s) const
+{
+	while (*s)
+		if (key_up(*s++))
+			return true;
+	return false;
+}
+
+bool glfw3_layer::user_input::mouse(button b) const
+{
+	return _mouse_buttons[(int)b];
+}
+
+bool glfw3_layer::user_input::mouse_up(button b) const
+{
+	return _mouse_buttons_up[(int)b];
+}
+
+bool glfw3_layer::user_input::mouse_wheel(wheel w) const
+{
+	return _mouse_buttons_up[w == wheel::up ? (int)button::wheel_up : (int)button::wheel_down];
+}
+
+ivec2 const & glfw3_layer::user_input::mouse_position() const
+{
+	return _mouse_pos;
+}
+
+void glfw3_layer::user_input::update()
+{
+	for (bool & k : _keys_up)  // set all keys up to false
+		k = false;
+
+	for (bool & b : _mouse_buttons_up)  // set all buttons up to false
+		b = false;
+}
+
+void glfw3_layer::user_input::mouse_motion(int x, int y)
+{
+	_mouse_pos = glm::ivec2{x,y};
+}
+
+void glfw3_layer::user_input::mouse_passive_motion(int x, int y)
+{
+	_mouse_pos = glm::ivec2{x,y};
+}
+
+void glfw3_layer::user_input::mouse_click(event_handler::button b, event_handler::state s, event_handler::modifier m, int x, int y)
+{
+	_mouse_pos = glm::ivec2{x,y};
+
+	if (s == event_handler::state::down)
+		_mouse_buttons[(int)b] = true;
+	else
+	{
+		_mouse_buttons[(int)b] = false;
+		_mouse_buttons_up[(int)b] = true;
+	}
+}
+
+void glfw3_layer::user_input::mouse_wheel(event_handler::wheel w, event_handler::modifier m, int x, int y)
+{
+	using eh = event_handler;
+
+	if (w == eh::wheel::up)
+		_mouse_buttons_up[(int)eh::button::wheel_up] = true;
+
+	if (w == eh::wheel::down)
+		_mouse_buttons_up[(int)eh::button::wheel_down] = true;
+}
+
+void glfw3_layer::user_input::key_typed(unsigned char c, event_handler::modifier m, int x, int y)
+{
+	std::clog << "key_typed(c=" << c << ")" << std::endl;
+
+	_keys[c] = true;
+}
+
+void glfw3_layer::user_input::key_released(unsigned char c, event_handler::modifier m, int x, int y)
+{
+	_keys[c] = false;
+	_keys_up[c] = true;
+}
+
+void glfw3_layer::user_input::touch_performed(int x, int y, int finger_id, event_handler::action a)
+{}
+
 
 namespace glfw_detail {
 
--- gles2_/gl/glfw3_window.hpp
+++ gles2_/gl/glfw3_window.hpp
@@ -17,16 +17,39 @@ public:
 	class user_input
 	{
 	public:
-		void update() {}  //!< for internal use only
+		user_input();
+
+		// verejne rozhranie
+		bool key(unsigned char c) const;
+		bool key_up(unsigned char c) const;
+		bool any_of_key(char const * s) const;
+		bool any_of_key_up(char const * s) const;
+		bool mouse(button b) const;
+		bool mouse_up(button b) const;
+		bool mouse_wheel(wheel w) const;
+		glm::ivec2 const & mouse_position() const;
+		// TODO: special keys support
+
+		void update();  //!< for internal use only
 
 		// povinne rozhranie
-		void mouse_motion(int x, int y) {}
-		void mouse_passive_motion(int x, int y) {}
-		void mouse_click(event_handler::button b, event_handler::state s, event_handler::modifier m, int x, int y) {}
-		void mouse_wheel(event_handler::wheel w, event_handler::modifier m, int x, int y) {}
-		void key_typed(unsigned char c, event_handler::modifier m, int x, int y) {}
-		void key_released(unsigned char c, event_handler::modifier m, int x, int y) {}
-		void touch_performed(int x, int y, int finger_id, event_handler::action a) {}
+		void mouse_motion(int x, int y);
+		void mouse_passive_motion(int x, int y);
+		void mouse_click(event_handler::button b, event_handler::state s, event_handler::modifier m, int x, int y);
+		void mouse_wheel(event_handler::wheel w, event_handler::modifier m, int x, int y);
+		void key_typed(unsigned char c, event_handler::modifier m, int x, int y);
+		void key_released(unsigned char c, event_handler::modifier m, int x, int y);
+		void touch_performed(int x, int y, int finger_id, event_handler::action a);
+
+	private:
+		static unsigned const NUM_KEYS = 256;
+		static unsigned const NUM_BUTTONS = (unsigned)button::number_of_buttons;
+
+		bool _keys[NUM_KEYS];
+		bool _keys_up[NUM_KEYS];
+		bool _mouse_buttons[NUM_BUTTONS];
+		bool _mouse_buttons_up[NUM_BUTTONS];
+		glm::ivec2 _mouse_pos;
 	};
 };
 
