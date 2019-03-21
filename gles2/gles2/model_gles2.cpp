89e7e71f34991d0ebb720b889552e75bb48abc88
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2019-03-21 17:24:19 +0100

    gles2: shadertoy update (uniform_variable(), gles2::property, ui::label, ...)

--- gles2/gles2/model_gles2.cpp
+++ gles2/gles2/model_gles2.cpp
@@ -8,7 +8,6 @@ using std::swap;
 using std::move;
 using std::shared_ptr;
 using std::string;
-using glm::vec3;
 using shader::program;
 
 
@@ -49,7 +48,7 @@ void model::append_global(property * prop)
 	_glob_props.push_back(prop);
 }
 
-void model::render(shader::program & p) const
+void model::render(program & p) const
 {
 	for (size_t i = 0; i < _meshes.size(); ++i)
 	{
@@ -67,28 +66,6 @@ void model::attribute_location(std::initializer_list<int> locations)
 {
 	for (auto m : _meshes)
 		m->attribute_location(locations);
-}
-
-
-texture_property::texture_property(shared_ptr<texture2d> tex, string const & uname, unsigned bind_unit)
-	: _tex{tex}, _uname{uname}, _bind_unit{bind_unit}
-{}
-
-void texture_property::bind(program & p)
-{
-	_tex->bind(_bind_unit);
-	p.uniform_variable(_uname, (int)_bind_unit);
-}
-
-material_property::material_property(vec3 const & ambient, float intensity, float shininess)
-	: ambient{ambient}, intensity{intensity}, shininess{shininess}
-{}
-
-void material_property::bind(program & p)
-{
-	p.uniform_variable("material_ambient", ambient);
-	p.uniform_variable("material_intensity", intensity);
-	p.uniform_variable("material_shininess", shininess);
 }
 
 }  // gles2
